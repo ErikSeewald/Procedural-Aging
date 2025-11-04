5ec597e88ec09a9ff574561982983e333bd6cc68
ErikSeewald <erik.seewald.g@gmail.com>
2025-11-04 17:23:38 +0100

    more dock demo

---
+++ scripts/docks/aging_demo.gd
@@ -0,0 +1,40 @@
+extends Node
+
+@export var demo_name: String
+@export var mesh_path: NodePath
+@export var material_slot: int
+
+@onready var mesh: MeshInstance3D = get_node(mesh_path)
+@onready var podium: Podium = $podium
+@onready var ui: Control = $DemoUI
+@onready var age_label: Label = $DemoUI/MarginContainer/VBoxContainer/AgeLabel
+@onready var demo_name_label: Label = $DemoUI/MarginContainer/VBoxContainer/DemoName
+@onready var age_slider: HSlider = $DemoUI/MarginContainer/VBoxContainer/HSlider
+
+var _mat: ShaderMaterial
+
+var _active = false
+
+func _ready() -> void:
+	add_to_group("aging_demos")
+	ui.visible = false
+	demo_name_label.text = demo_name
+	age_slider.value_changed.connect(set_age)
+	podium.button_pressed.connect(_on_podium_press)
+	_mat = mesh.get_surface_override_material(material_slot)
+
+func _on_podium_press() -> void:
+	set_activation(!_active)
+	if _active:
+		for n in get_tree().get_nodes_in_group("aging_demos"):
+			if n == self:
+				continue
+			n.set_activation(false)
+
+func set_activation(active: bool) -> void:
+	_active = active
+	ui.visible = _active
+	podium.switch_material(_active)
+
+func set_age(age: float) -> void:
+	_mat.set_shader_parameter("age", age)
--- scripts/docks/podium.gd
+++ scripts/docks/podium.gd
@@ -2,13 +2,35 @@ extends Node3D
 class_name Podium
 
 @onready var button_area := $Button/Area3D
+@onready var podium_mesh := $Podium
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var default_texture: Texture2D
+@export var activated_texture: Texture2D
+@export var click_sound: AudioStream
+
+var _podium_mat: StandardMaterial3D
+var _sfx_player: AudioStreamPlayer
 
 signal button_pressed
 
 func _ready() -> void:
+	_podium_mat = podium_mesh.get_active_material(0)
+	_podium_mat.albedo_texture = default_texture
 	button_area.input_event.connect(_on_input)
+	
+	_sfx_player = AudioStreamPlayer.new()
+	_sfx_player.stream = click_sound
+	add_child(_sfx_player)
 
 func _on_input(_camera: Node, event: InputEvent, _event_position: Vector3, 
 _normal: Vector3,  _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		button_pressed.emit()
+		animation_player.play("ButtonPressAction")
+		_sfx_player.play()
+		
+func switch_material(is_active: bool) -> void:
+	if is_active:
+		_podium_mat.albedo_texture = activated_texture
+	else:
+		_podium_mat.albedo_texture = default_texture
