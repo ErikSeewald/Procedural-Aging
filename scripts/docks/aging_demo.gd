e6e39fbe8c8a8ab6c96fa60ac729d80ac25c74b3
ErikSeewald <erik.seewald.g@gmail.com>
2025-11-05 19:52:06 +0100

    more demo work

--- scripts/docks/aging_demo.gd
+++ scripts/docks/aging_demo.gd
@@ -10,6 +10,7 @@ extends Node
 @onready var age_label: Label = $DemoUI/MarginContainer/VBoxContainer/AgeLabel
 @onready var demo_name_label: Label = $DemoUI/MarginContainer/VBoxContainer/DemoName
 @onready var age_slider: HSlider = $DemoUI/MarginContainer/VBoxContainer/HSlider
+@onready var spotlight: SpotLight3D = $SpotLight3D
 
 var _mat: ShaderMaterial
 
@@ -22,6 +23,7 @@ func _ready() -> void:
 	age_slider.value_changed.connect(set_age)
 	podium.button_pressed.connect(_on_podium_press)
 	_mat = mesh.get_surface_override_material(material_slot)
+	age_slider.set_value_no_signal(_mat.get_shader_parameter("age"))
 
 func _on_podium_press() -> void:
 	set_activation(!_active)
@@ -34,6 +36,7 @@ func _on_podium_press() -> void:
 func set_activation(active: bool) -> void:
 	_active = active
 	ui.visible = _active
+	spotlight.visible = _active
 	podium.switch_material(_active)
 
 func set_age(age: float) -> void:
